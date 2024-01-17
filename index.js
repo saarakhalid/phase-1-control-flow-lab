8a901fee394462757771a8d0964420a21ad7bd54
saarakhalid <khalidsaara004@gmail.com>
2024-01-17 15:14:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-function scuberGreetingForFeet(distance){
-  if (distance <= 400) {
+// index.js
+
+// Function to determine the greeting based on distance
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
     return 'This one is on me!';
-  } else if (distance > 2000 && distance <= 2500) {
+  } else if (someValue > 2000 && someValue <= 2500) {
     return 'I will gladly take your thirty bucks.';
-  } else if (distance > 2500) {
+  } else if (someValue > 2500) {
     return 'No can do.';
   } else {
     return 'This one is on me!';
   }
 }
 
-function ternaryCheckCity(city){
+// Function to check the city using a ternary operator
+function ternaryCheckCity(city) {
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-
-function switchOnCharmFromTip(tip){
-  
-
+// Function to determine response based on tip generosity using a switch statement
+function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
       return 'Thank you so much.';
@@ -26,11 +28,11 @@ function switchOnCharmFromTip(tip){
     default:
       return 'Bye.';
   }
-
-}module.exports = {
-  scuberGreetingForFeet,
-  ternaryCheckCity,
-  switchOnCharmFromTip,
-};
+}
 
 
+
+// Example test cases:
+console.log(scuberGreetingForFeet(199)); // Output: 'This one is on me!'
+console.log(ternaryCheckCity('NYC')); // Output: 'Ok, sounds good.'
+console.log(switchOnCharmFromTip('generous')); // Output: 'Thank you so much.'
